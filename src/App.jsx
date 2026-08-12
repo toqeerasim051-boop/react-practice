8e45e4a48f50a1ff57ba2efc3a6c7f2166f07011
Touqeer Asim <your_github_email@example.com>
2026-08-12 19:04:53 +0500

    Add BgChanger Project

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+
+function App() {
+  const [color,setColor]=useState("olive");
+  return (
+    <div className="w-full h-screen duration-200"
+       style={{backgroundColor:color}}>
+        <div className="fixed flex flex-wrap justify-center
+        bottom-12 inset-x-0 px-2">
+          <div className="flex justify-center flex-wrap gap-3 shadow-lg bg-white
+          px-3 py-2 rounded-3xl">
+            <button
+            onClick={()=>setColor("red")}
+             className="outline-none px-4 py-1
+            rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor:"red"}}>Red</button>
+            <button 
+             onClick={()=>setColor("green")}
+            className="outline-none px-4 py-1
+            rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor:"green"}}>Green</button>
+            <button
+             onClick={()=>setColor("blue")}
+             className="outline-none px-4 py-1
+            rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor:"blue"}}>Blue</button>
+            <button 
+             onClick={()=>setColor("purple")}
+            className="outline-none px-4 py-1
+            rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor:"purple"}}>Purple</button>
+            <button
+             onClick={()=>setColor("white")}
+             className="outline-none px-4 py-1
+            rounded-full text-black shadow-lg cursor-pointer"
+            style={{backgroundColor:"white"}}>White</button>
+            <button
+             onClick={()=>setColor("black")}
+             className="outline-none px-4 py-1
+            rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor:"black"}}>Black</button>
+            <button
+              onClick={()=>setColor("brown")} 
+             className="outline-none px-4 py-1
+            rounded-full text-white shadow-lg cursor-pointer"
+            style={{backgroundColor:"brown"}}>Brown</button>
+            
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
+
+
+// here we passed callback functions in onclick because onclick(accept function not return value)
